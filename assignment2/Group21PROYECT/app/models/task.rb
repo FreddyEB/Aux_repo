39809757453602332cd93a6f5e5c5d2564f9a6a6
Freddy <febacigalupo1@miuandes.cl>
2024-05-02 11:23:01 -0400

    Latest Validations fix

--- assignment2/Group21PROYECT/app/models/task.rb
+++ assignment2/Group21PROYECT/app/models/task.rb
@@ -4,10 +4,10 @@ class Task < ApplicationRecord
     belongs_to :state
 
     validates :title, presence: { message: "This field can't be blank" }, uniqueness: { message: "This task already exists" }
-    validates :priority, presence:{ message: "Your board needs a priority level"}
-    validates :user_id, presence:{ message: "Your board needs an associated User"}
-    validates :state_id, presence:{ message: "Your board needs to be associated to a State"}
-    validates :asignee_task_id, presence:{ message: "Your board needs an Assignee Id"}
+    validates :priority, presence:{ message: "Your task needs a priority level"}
+    validates :user_id, presence:{ message: "Your task needs an associated User"}
+    validates :state_id, presence:{ message: "Your task needs to be associated to a State"}
+    validates :asignee_task_id, presence:{ message: "Your task needs an Assignee Id"}
 
     before_validation :normalize_title
     private
