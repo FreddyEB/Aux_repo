0d1d51570f446c9d98fa440fe5c854ff0ac90722
Freddy <febacigalupo1@miuandes.cl>
2024-05-23 11:17:59 -0400

    Fixes

--- assignment2/Group21PROYECT/app/controllers/boards_controller.rb
+++ assignment2/Group21PROYECT/app/controllers/boards_controller.rb
@@ -6,9 +6,11 @@ class BoardsController < ApplicationController
 
     def show
         @board = Board.find(params['id'])
-        @state = State.find(params['id'])
         @states = @board.states
-        @tasks = @state.tasks
+        @tasks = []
+        @states.each do |state| 
+            @tasks << state.tasks
+        end
     end
 
     def new
